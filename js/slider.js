6e9fc650f5c6ee46c1a3b7f61f31a1898fe3bbe2
Vi Snyder <ViTheAxolotl@gmail.com>
2023-02-08 20:55:43 -0500

    AHHHHHHH

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,36 @@
 "use strict";
-let slider = document.getElementById("myRange");
+
+let slider = document.getElementById("angle");
 let output = document.getElementById("demo");
 output.innerHTML = slider.value;
 
 slider.oninput = function() 
 {
-  output.innerHTML = this.value;
+  output.innerHTML = Math.abs(this.value);
+  adjustRotation(this.value);
 }
+
+function adjustRotation(newAngle)
+{ 
+  let laserBeamL = document.getElementById("laserBeamL");
+  let laser = document.getElementById("laser");
+  let laserBeamR = document.getElementById("laserBeamR");
+  laser.style.transform = "rotate(" + newAngle + "deg)";
+
+  if(newAngle > 0)
+  {
+    laserBeamL.setAttribute("class", "hide");
+    laserBeamR.setAttribute("class", "unhide center");
+    laserBeamR.style.transform = "rotate(" + (-newAngle) + "deg)";
+    //Activate Right Lazer and turn
+  }
+
+  if(newAngle < 0)
+  {
+    laserBeamR.setAttribute("class", "hide");
+    laserBeamL.setAttribute("class", "unhide center");
+    laserBeamL.style.transform = "rotate(" + (-newAngle) + "deg)";
+    //Activate Left lazer and turn
+  }
+}
+
