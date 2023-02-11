9ed1082cbbb019a5867335014784f3171a9527fa
Vi Snyder <ViTheAxolotl@gmail.com>
2023-02-11 14:23:08 -0500

    Finished

--- js/NavBar.js
+++ js/NavBar.js
@@ -3,7 +3,7 @@ document.write(
     '<div class="collapse navbar-collapse" id="navbarSupportedContent"> ' +
         '<ul class="navbar-nav me-auto my-2 my-lg-0 center" style="--bs-scroll-height: 100px;"> ' +
             '<li class="nav-item"><a class="nav-link active" aria-current="page" href="index.html">Reflection Demo</a></li>' +
-            '<li class="nav-item"><a class="nav-link active" aria-current="page" href="refractionDemo.html">Reflaction Demo</a></li>' + 
+            '<li class="nav-item"><a class="nav-link active" aria-current="page" href="refractionDemo.html">Refraction Demo</a></li>' + 
         '</ul>' +
     '</div>' +
 '</div>');
